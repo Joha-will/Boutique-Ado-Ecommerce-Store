27fd814a780fab3ed5f6a5d9900bc489b80b2b0b
Joha-will <johawilsonatu@icloud.com>
2023-07-16 20:42:31 +0000

    Added notification css, shopping bag preview and additional messages

--- products/views.py
+++ products/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render,get_object_or_404, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category
 
+# Create your views here.
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
-
     query = None
     categories = None
     sort = None
@@ -22,7 +23,6 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-            
             if sortkey == 'category':
                 sortkey = 'category__name'
             if 'direction' in request.GET:
@@ -30,19 +30,18 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-    if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, 'You did not enter any search criteria!')
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
@@ -51,7 +50,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
-        'current_categories':categories,
+        'current_categories': categories,
         'current_sorting': current_sorting,
     }
 

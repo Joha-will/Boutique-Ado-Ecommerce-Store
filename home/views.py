ab4a7a6e0b5983ebd11acd9372d95acbcd49391a
Joha-will <johawilsonatu@icloud.com>
2023-08-04 15:37:11 +0000

    Added 404 error handling functionality

--- home/views.py
+++ home/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def index(request):
     """ A view to render index page"""
     return render(request, 'home/index.html')
+
+
+def handler404(request, exception):
+    """ Error Handler 404 - Page Not Found """
+    return render(request, "errors/404.html", status=404)
+
+
+def handler500(request):
+    """ Error Handler 500 """
+    return render(request, "errors/500.html", status=500)
